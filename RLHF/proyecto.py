565afe2b487bd839d9b11e39846235c76f8387fe
MirioVasquez <ecarchive13@gmail.com>
2025-02-05 13:01:29 -0500

    Update proyecto.py and delete ReadMe

--- RLHF/proyecto.py
+++ RLHF/proyecto.py
@@ -1,11 +1,10 @@
-def reverse_number(num):
+def reverse_number(cab):
   # Reverse the number
-  num1 = str(num)
+  num1 = str(cab)
   reverse = num1[::-1]
   reverse1 = int(reverse)
   # Return the number
   return reverse1
-
 ## Example usage:
-print(type(reverse_number(1234))) # Output: 3221
+print(f"El numero es: {reverse_number(1234)}") # Output: 3221
 #print(reverse_number(987654321)) # Output: 123456789
